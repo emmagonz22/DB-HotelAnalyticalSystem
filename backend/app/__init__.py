36a25062bd183a56fa847cb177f28e9be730f06d
Emmanuel Gonzalez Morales <emmanuel.gonzalez22@upr.edu>
2024-03-26 09:43:42 -0400

    Removing config temporary

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask     # https://flask.palletsprojects.com/en/3.0.x/
 import logging              # https://docs.python.org/3/library/logging.html
-from .config import config
+#from .config import config
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -10,7 +10,7 @@ def create_app():
 
     # Config file path (Optional Todo: create a config for test and another one for production)
     # app.config.from_pyfile('config.py', silent=True) To load from python file
-    app.config.from_object(config)  # to load config from class
+    #app.config.from_object(config)  # to load config from class
 
 
     # create end points with from the views
--- backend/main.py
+++ backend/main.py
@@ -9,4 +9,4 @@ def index(): # Temporary index endpoint
 
 if __name__ == "__main__":
   
-    app.run(host="0.0.0.0", port=app.config['PORT'])
+    app.run()
