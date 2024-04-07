a12f154c267dd77b3a88a0364725e5aab56983f1
RicardoMorell <ricardo.morell1@upr.edu>
2024-04-07 12:53:59 -0400

    Implemented GetAll, GetbyId and Create for Chains

--- backend/app/models/chains.py
+++ backend/app/models/chains.py
@@ -7,4 +7,20 @@ class ChainsDAO(BaseDAO):
         result = []
         for row in cur:
             result.append(dict(zip(["chid", "cname", "springmkup", "summermkup", "fallmkup", "wintermkup"], row)))
+        self.conn.close()
+        return result
+
+    def getChainsbyId(self, chid):
+        cur = self.conn.cursor()
+        cur.execute("SELECT chid, cname, springmkup, summermkup, fallmkup, wintermkup from chains where chid = %s;", (chid,))
+        result = dict(zip(["chid", "cname", "springmkup", "summermkup", "fallmkup", "wintermkup"], cur.fetchone()))
+        self.conn.close()
+        return result
+    
+    def createChain(self, data):
+        cur = self.conn.cursor()
+        cur.execute("INSERT into chains (cname, springmkup, summermkup, fallmkup, wintermkup) values (%s, %s, %s, %s, %s, %s) returning chid;",
+                    (data["cname"], data["springmkup"], data["summermkup"], data["fallmkup"], data["wintermkup"]))
+        result = cur.fetchone()[0]
+        self.conn.close()
         return result
