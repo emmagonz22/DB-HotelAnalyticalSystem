5da8ec2fb458dd490d4c20b7ad10db9f7fa33be3
diego-green <diego.green@upr.edu>
2024-04-07 23:41:49 -0400

    Implemented CRUD operations for Employee

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.DEBUG)
 def create_app():
     # instance_relative_config is used to determine where the conguration files are stored, this is used for the local database files, configuration secrets, instance specific data
     app = Flask(__name__, instance_relative_config=True) 
-
+    app.json.sort_keys = False
     # Config file path (Optional Todo: create a config for test and another one for production)
     # app.config.from_pyfile('config.py', silent=True) To load from python file
     #app.config.from_object(config)  # to load config from class
