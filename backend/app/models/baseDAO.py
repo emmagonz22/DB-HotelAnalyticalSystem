7b476b90f10cf7ddf13c5a1028e9c370c69b3201
emmagonz22 <emmanuel.gonzalez22@upr.edu>
2024-04-10 00:42:32 -0400

    Debug print basedao

--- backend/app/models/baseDAO.py
+++ backend/app/models/baseDAO.py
@@ -5,19 +5,19 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
-#dotenv_path = os.path.abspath("backend/app/enviroment.env")
-dotenv_path = os.path.abspath("app/enviroment_prod.env")
+dotenv_path = os.path.abspath("backend/app/enviroment.env")
+#dotenv_path = os.path.abspath("app/enviroment_prod.env")
 print(dotenv_path)
 load_dotenv(dotenv_path=dotenv_path)
-print( os.getenv('DATABASE'),
-        os.getenv('USER_DB'),
-        os.getenv('PASSWORD'),
-        os.getenv('HOST'),
-        os.getenv('PORT'))
+
 class BaseDAO:
 
     def __init__(self):
-
+    print( os.getenv('DATABASE'),
+            os.getenv('USER_DB'),
+            os.getenv('PASSWORD'),
+            os.getenv('HOST'),
+            os.getenv('PORT'))
         self.conn = psycopg2.connect(
             database=os.getenv('DATABASE'),
             user=os.getenv('USER_DB'),
