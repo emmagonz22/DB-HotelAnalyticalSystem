1c986f56548a5d939f06d5ade778543dc776ccef
diego-green <diego.green@upr.edu>
2024-05-11 22:29:02 -0400

    CRUD for 4 more tables

--- frontend/notebooks/api/entityCRUD.py
+++ frontend/notebooks/api/entityCRUD.py
@@ -43,3 +43,59 @@ def updateEmployee(info, id):
 def deleteEmployee(id):
     headers = {'Content-Type': 'application/json'}
     return requests.delete(url + f"employee/{id}", headers=headers)
+
+
+# Login
+def createLogin(info):
+    headers = {'Content-Type': 'application/json'}
+    return requests.post(url + "login", json=info, headers=headers)
+
+def updateLogin(info, id):
+    headers = {'Content-Type': 'application/json'}
+    return requests.put(url + f"login/{id}", json=info, headers=headers)
+
+def deleteLogin(id):
+    headers = {'Content-Type': 'application/json'}
+    return requests.delete(url + f"login/{id}", headers=headers)
+
+
+# Room
+def createRoom(info):
+    headers = {'Content-Type': 'application/json'}
+    return requests.post(url + "room", json=info, headers=headers)
+
+def updateRoom(info, id):
+    headers = {'Content-Type': 'application/json'}
+    return requests.put(url + f"room/{id}", json=info, headers=headers)
+
+def deleteRoom(id):
+    headers = {'Content-Type': 'application/json'}
+    return requests.delete(url + f"room/{id}", headers=headers)
+
+
+# Client
+def createClient(info):
+    headers = {'Content-Type': 'application/json'}
+    return requests.post(url + "client", json=info, headers=headers)
+
+def updateClient(info, id):
+    headers = {'Content-Type': 'application/json'}
+    return requests.put(url + f"client/{id}", json=info, headers=headers)
+
+def deleteClient(id):
+    headers = {'Content-Type': 'application/json'}
+    return requests.delete(url + f"client/{id}", headers=headers)
+
+
+# Reserve
+def createReserve(info):
+    headers = {'Content-Type': 'application/json'}
+    return requests.post(url + "reserve", json=info, headers=headers)
+
+def updateReserve(info, id):
+    headers = {'Content-Type': 'application/json'}
+    return requests.put(url + f"reserve/{id}", json=info, headers=headers)
+
+def deleteReserve(id):
+    headers = {'Content-Type': 'application/json'}
+    return requests.delete(url + f"reserve/{id}", headers=headers)
