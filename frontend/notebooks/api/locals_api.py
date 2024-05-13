f4246f68ac570bf87972c4bba46a0fee405e4b5c
emmagonz22 <emmanuel.gonzalez22@upr.edu>
2024-05-12 23:03:12 -0400

    Production variables

--- frontend/notebooks/api/locals_api.py
+++ frontend/notebooks/api/locals_api.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:5000/"
+url = "https://db-hotel-analytics-systems-6a60248fdbac.herokuapp.com/"
 
 def TopFiveRoomHandicap(hid, info):
     headers = {'Content-Type': 'application/json'}
